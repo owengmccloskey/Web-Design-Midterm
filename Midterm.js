34a211dc656d1b373606e0869db2014f4d12d5b2
owengmccloskey <60757566+owengmccloskey@users.noreply.github.com>
2020-03-24 18:44:06 -0400

    Add files via upload

---
+++ Midterm.js
@@ -0,0 +1,10 @@
+var c = canvas.getContext('2d');
+//Draw Rectangle//
+//to fill the rectangle you use fillstyle
+c.fillStyle="green";
+/*c.fillRect(x,y,width,height);*/
+c.fillRect(100,100,100,100);
+c.fillStyle="green";
+c.fillRect(200,200,100,100);
+c.fillStyle="green";
+c.fillRect(300,300,100,100)
